91551ff7bb21bfb14560be10c4216c2f9f5436b3
yuppejp <yuppejp@gmail.com>
2023-09-18 11:22:49 +0900

    Initial Commit

---
+++ FancyTextSample/FancyTextSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  FancyTextSampleApp.swift
+//  FancyTextSample
+//
+//  Created by main on 2023/09/18.
+//
+
+import SwiftUI
+
+@main
+struct FancyTextSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
